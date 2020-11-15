5abbd47b608801664f3a382c6e06e6e6cda3b60e
Explicious <46688743+Explicious@users.noreply.github.com>
2020-11-15 03:13:16 -0600

    Added reading from raw html forms

--- hack-ohio-2020/website/userIn/views.py
+++ hack-ohio-2020/website/userIn/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.template import loader
 
+from .models import Weights
+
 # Create your views here.
 
 def index(request):
     template = loader.get_template('userIn/index.html')
-    context = {'e':request.POST.get('environment'),
-               't':request.POST.get('time'),
-               'd':request.POST.get('distance'),
+    if(len(request.POST)==1):
+        context = {'e':"50",
+                   'ti':"50",
+                   'tr':"50",
+                   'c':"50",}
+
+    context = {'e':request.POST.get('enviro'),
+               'ti':request.POST.get('time'),
+               'tr':request.POST.get('truck'),
                'c':request.POST.get('cost'),}
-    print(request.POST.get('environment'))
+    print(context)
+
     return render(request, 'userIn/index.html', context)
