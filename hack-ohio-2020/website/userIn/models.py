99d4412b482092ec75fb6d595c7706a98fc23b62
Explicious <46688743+Explicious@users.noreply.github.com>
2020-11-14 20:17:42 -0600

    Added model for weights and waypoints

--- hack-ohio-2020/website/userIn/models.py
+++ hack-ohio-2020/website/userIn/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Weights(models.Model):
+    environment = models.IntegerField(blank = False)
+    time = models.IntegerField(blank = False)
+    trucks = models.IntegerField(blank = False)
+    distance = models.IntegerField(blank = False)
+
+class Waypoint(models.Model):
+    street = models.CharField(max_length = 120, blank = False)
+    building_num = models.CharField(max_length = 8, blank = False)
+    state = models.CharField(max_length = 120, blank = False)
+    zipcode = models.CharField(max_length = 10, blank = False)
+    is_warehouse = models.BooleanField()
--- hack-ohio-2020/website/website/settings.py
+++ hack-ohio-2020/website/website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userIn',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'CST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
